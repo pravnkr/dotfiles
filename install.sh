4586d5f7384f7c316769ec6d782411cbb6bde651
praveen kumar <praveenkumar8484@outlook.com>
2024-03-04 10:19:32 +0000

    initial p10k config added

--- install.sh
+++ install.sh
@@ -13,3 +13,29 @@ sed -i '/^source "\$ZSH\/oh-my-zsh.sh"/i fpath+=${ZSH_CUSTOM:-${ZSH:-~\/.oh-my-z
 sed -i 's/plugins=(git)/plugins=(git zsh-autosuggestions zsh-syntax-highlighting)/' ~/.zshrc
 
 sed -i '/^ZSH_THEME/c\ZSH_THEME="powerlevel10k/powerlevel10k"' ~/.zshrc
+
+# Create a temporary file
+temp_file=$(mktemp)
+
+# Write the instant prompt enable code to the temporary file
+cat <<'EOF' > "$temp_file"
+# Enable Powerlevel10k instant prompt. Should stay close to the top of ~/.zshrc.
+# Initialization code that may require console input (password prompts, [y/n]
+# confirmations, etc.) must go above this block; everything else may go below.
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+  source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+EOF
+
+# Append the original .zshrc content to the temporary file
+cat ~/.zshrc >> "$temp_file"
+
+# Move the temporary file to replace .zshrc
+mv "$temp_file" ~/.zshrc
+
+# Append the p10k configuration note to the end of .zshrc
+cat <<'EOF' >> ~/.zshrc
+
+# To customize prompt, run `p10k configure` or edit ~/.p10k.zsh.
+[[ ! -f ~/.oh-my-zsh/custom/themes/powerlevel10k/config/p10k-lean-8colors.zsh ]] || source ~/.oh-my-zsh/custom/themes/powerlevel10k/config/p10k-lean-8colors.zsh
+EOF
